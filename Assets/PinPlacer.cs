5102eba97b3a45cb9657f43d41a43632e4a0949b
Sailor Usher <100800088+Joseph-Sailor-Usher@users.noreply.github.com>
2023-09-13 16:29:20 -0500

    Updated the pin placement system

--- Assets/PinPlacer.cs
+++ Assets/PinPlacer.cs
@@ -1,19 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public struct Location
+{
+    public float longitude, latitude;
+    public string name;
+}
+
+[Serializable]
+public class LocationList
+{
+    public List<Location> locations;
+}
+
 public class PinPlacer : MonoBehaviour
 {
     public GameObject pinPrefab;
-    public List<GameObject> pins;
+    public Vector3 globeCenter;
+    public LocationList locationList; // Use the custom wrapper class
+    public List<GameObject> activePins;
 
     private void Start()
     {
-        pins = new();
+        activePins = new List<GameObject>();
+        foreach (Location location in locationList.locations)
+        {
+            PlacePin(location.longitude, location.latitude);
+        }
     }
 
     public void PlacePin(float latitude, float longitude)
     {
-        
+        GameObject newPin = Instantiate(pinPrefab, globeCenter, Quaternion.identity, this.transform);
+        Vector3 tempRotation = new Vector3(0, longitude, latitude);
+        newPin.transform.rotation = Quaternion.Euler(tempRotation);
     }
 }
